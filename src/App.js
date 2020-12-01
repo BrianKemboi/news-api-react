bd13603ed544ea641ccca5f0030a3d7d4472a635
BrianKemboi <22851850+BrianKemboi@users.noreply.github.com>
2020-12-01 22:02:41 +0300

    app Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ArticlesContainer from './containers/ArticlesContainer'
+
+const App = () => {
+    return (
+        <div>
+            <ArticlesContainer/>
+        </div>
+    )
+}
+
+export default App
